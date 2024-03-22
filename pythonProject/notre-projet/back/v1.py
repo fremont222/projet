4fd75325498edd9604f4acefe460c247e51c388a
fremont222 <162692173+fremont222@users.noreply.github.com>
2024-03-22 16:00:39 +0100

    Merge remote-tracking branch 'origin/main'

--- pythonProject/notre-projet/back/v1.py
+++ pythonProject/notre-projet/back/v1.py
@@ -1,11 +1,21 @@
 from flask import Flask, url_for, request, render_template, redirect
 app = Flask(__name__)
+import sqlite3 as lite
 @app.route('/', methods=['GET'])
 def index():
 	return render_template('page0.html');
 @app.route('/Agilean', methods=['GET','POST'])
 def Agilean():
+	con = lite.connect('projet_test.db')
+	con.row_factory = lite.Row
+	cur = con.cursor()
 	return render_template('Agilean.html');
+	"""
+	cur.execute("SELECT code_voiture FROM voiture_dans_commande")
+	lignes = cur.fetchall()
+	con.close()
+	return render_template('Agilean.html',pieces=lignes)
+	"""
 @app.route('/Agilog', methods=['GET','POST'])
 def Agilog():
 	return render_template('Agilog.html');
