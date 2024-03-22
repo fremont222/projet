9343836cd4a8005ee7ecd16330bdda2530ee93d4
fremont222 <162692173+fremont222@users.noreply.github.com>
2024-03-22 17:39:02 +0100

    Merge remote-tracking branch 'origin/main'

--- pythonProject/notre-projet/back/v1.py
+++ pythonProject/notre-projet/back/v1.py
@@ -6,16 +6,20 @@ def index():
 	return render_template('page0.html');
 @app.route('/Agilean', methods=['GET','POST'])
 def Agilean():
-	con = lite.connect('projet_test.db')
-	con.row_factory = lite.Row
-	cur = con.cursor()
-	return render_template('Agilean.html');
+	con = lite.connect(r'C:\Users\agath\Desktop\2A\Maths info\Base de donnée- DBBrowser\projet_test.db')
+	pieces = con.execute('SELECT code_voiture FROM voiture_dans_commande').fetchall()
+	con.close()
+	print(pieces)
+	return render_template('Agilean.html',pieces=pieces);
+
 	"""
 	cur.execute("SELECT code_voiture FROM voiture_dans_commande")
 	lignes = cur.fetchall()
+	con.commit()
 	con.close()
 	return render_template('Agilean.html',pieces=lignes)
 	"""
+
 @app.route('/Agilog', methods=['GET','POST'])
 def Agilog():
 	return render_template('Agilog.html');
