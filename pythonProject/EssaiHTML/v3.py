15a8bab53f45213b3506e199f329ce2edd7d970a
fremont222 <162692173+fremont222@users.noreply.github.com>
2024-04-17 00:38:34 +0200

    Merge remote-tracking branch 'origin/main'

--- pythonProject/EssaiHTML/v3.py
+++ pythonProject/EssaiHTML/v3.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+import sqlite3 as lite
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+type_voiture = ["CLF", "CLO", "CCF", "CCO"]
+
 @app.route('/')
 def index():
     return render_template('Code.html')
@@ -12,19 +17,32 @@ def Code():
 @app.route('/CLF', methods=['POST'])
 def CLF():
     # Code pour gérer la sélection de CLF
+    global a
+    a = type_voiture[0]
+    print(a)
     return render_template('CLF.html')
 
 @app.route('/CLO', methods=['POST'])
 def CLO():
     # Code pour gérer la sélection de CLO
+    global a
+    a = type_voiture[1]
+    print(a)
     return render_template('CLO.html')
+
 @app.route('/CCO', methods=['POST'])
 def CCO():
     # Code pour gérer la sélection de CCO
+    global a
+    a = type_voiture[2]
+    print(a)
     return render_template('CCO.html')
 @app.route('/CCF', methods=['POST'])
 def CCF():
     # Code pour gérer la sélection de CCF
+    global a
+    a = type_voiture[3]
+    print(a)
     return render_template('CCF.html')
 
 @app.route('/Agigreen', methods=['POST'])
@@ -38,6 +56,11 @@ def Agilog():
 
 @app.route('/Commande', methods=['POST'])
 def Commande():
+    type_voiture = session.get('type_voiture')
+    con = lite.connect('projet_test.db')
+    pieces = con.execute('SELECT code_voiture FROM voiture_dans_commande').fetchall()
+    con.close()
+    print(a)
     # Code pour gérer la sélection de Agilog
     return render_template('Commande.html')
 
